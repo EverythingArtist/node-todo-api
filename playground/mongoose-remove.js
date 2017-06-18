fbd2cfb4f29f2c6640cb221de0ca52b50d5c3585
Aishwarya Rana <ashrana1993@gmail.com>
2017-06-18 13:26:55 -0400

    Add delete todos by :id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,21 @@
+
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+//Todo.findOneAndRemove
+//Todo.findByIdAndRemove
+
+// Todo.findOneAndRemove({_id: '5946b0d52b02a4657adcd0c6'}).then((todo) =>{
+//   console.log(todo);
+// });
+
+
+Todo.findByIdAndRemove('5946b0d52b02a4657adcd0c6').then((todo) =>{
+  console.log(todo);
+});
